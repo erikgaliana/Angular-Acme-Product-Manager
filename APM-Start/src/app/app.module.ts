35b39557a13c469df8ca4f758c1e28c1964e7c71
Erik Galiana Farell <erikgalianafarell@MacBook-Pro-de-Erik.local>
2020-05-10 17:02:03 +0200

    add productListComponent

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ 
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
