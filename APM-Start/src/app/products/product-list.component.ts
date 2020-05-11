39601a4239fa0d4716c5fb4aa6eac478061d192e
Erik Galiana Farell <erikgalianafarell@MacBook-Pro-de-Erik.local>
2020-05-11 20:05:06 +0200

    ger data from observable http

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit {
   imageMargin = 2;
   showImage = false;
   _listFilter: string;
+  errorMessage: string;
   get listFilter(){
       return this._listFilter;
   }
@@ -44,8 +45,14 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+        error: err => this.errorMessage = err
+    });
+    
   }
 
 }
