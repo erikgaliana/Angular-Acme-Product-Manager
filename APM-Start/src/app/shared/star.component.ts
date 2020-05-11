2d1fb0a0f5ef9c5642f648fa356dc078059d544b
Erik Galiana Farell <erikgalianafarell@MacBook-Pro-de-Erik.local>
2020-05-11 16:27:13 +0200

    return info from starcomponent

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
     selector: 'pm-star',
@@ -8,13 +9,15 @@ import { Component, OnChanges, Input } from '@angular/core';
 export class StarComponent implements OnChanges {
     @Input() rating: number;
     starWidth: number;
+    @Output() ratingClicked: EventEmitter<string> =
+         new EventEmitter<string>();
 
     ngOnChanges(): void {
         this.starWidth = this.rating * 75 / 5;
     }
 
     onClick(): void {
-            console.log(` This rating ${this.rating} was clicked`);
+            this.ratingClicked.emit(` This rating ${this.rating} was clicked`);
 
     }
 }
